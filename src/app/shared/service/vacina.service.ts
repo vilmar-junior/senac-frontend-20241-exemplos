3c160d52ceebfcb3b2ec6fbed8f9225cc468ab47
Vilmar Pereira <vilmarcesarpereira@gmail.com>
2024-04-15 11:05:38 -0300

    Aula 7 - filtros com select (combobox)

--- src/app/shared/service/vacina.service.ts
+++ src/app/shared/service/vacina.service.ts
@@ -2,6 +2,7 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { Vacina } from '../model/vacina';
+import { VacinaSeletor } from '../model/seletor/vacina.seletor';
 
 @Injectable({
   providedIn: 'root'
@@ -16,7 +17,15 @@ export class VacinaService {
     return this.httpClient.get<Array<Vacina>>(this.API + "/todas");
   }
 
+  public consultarComSeletor(seletor: VacinaSeletor): Observable<Array<Vacina>>{
+    return this.httpClient.post<Array<Vacina>>(this.API + "/filtro", seletor);
+  }
+
   public consultarPorId(id: number): Observable<Vacina>{
     return this.httpClient.get<Vacina>(this.API + "/" + id);
+  }
+
+  public excluir(id: number): Observable<boolean>{
+    return this.httpClient.delete<boolean>(this.API + "/" + id);
   }
 }
--- src/app/vacinas/vacina-listagem/vacina-listagem.component.ts
+++ src/app/vacinas/vacina-listagem/vacina-listagem.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { VacinaSeletor } from '../../shared/model/seletor/vacina.seletor';
 import { Vacina } from '../../shared/model/vacina';
+import { PaisService } from '../../shared/service/pais.service';
 import { VacinaService } from '../../shared/service/vacina.service';
+import { Pais } from '../../shared/model/pais';
 
 @Component({
   selector: 'app-vacina-listagem',
@@ -12,14 +15,42 @@ import { VacinaService } from '../../shared/service/vacina.service';
 export class VacinaListagemComponent implements OnInit  {
 
   public vacinas: Array<Vacina> = new Array();
+  public seletor: VacinaSeletor = new VacinaSeletor();
+  public paises: Array<Pais> = new Array();
 
-  constructor(private vacinaService: VacinaService){}
-
-  ngOnInit(): void {
-    this.consultarTodas();
+  constructor(private vacinaService: VacinaService,
+              private paisService: PaisService){
   }
 
-  private consultarTodas(){
+  ngOnInit(): void {
+    this.consultarTodasVacinas();
+
+    this.paisService.consultarTodos().subscribe(
+      resultado => {
+        this.paises = resultado;
+      },
+      erro => {
+        console.log('Erro ao buscar países' + erro)
+      }
+    );
+  }
+
+  public pesquisar(){
+    this.vacinaService.consultarComSeletor(this.seletor).subscribe(
+      resultado => {
+        this.vacinas = resultado; 
+      },
+      erro => {
+        console.log('Erro ao buscar todas as vacinas' + erro)
+      }
+    )
+  }
+
+  limpar(){
+    this.seletor = new VacinaSeletor();
+  }
+
+  private consultarTodasVacinas(){
     this.vacinaService.consultarTodas().subscribe(
       resultado => {
          this.vacinas = resultado; 
